1b799e524017bd5387975256ba701043339dca16
DavidG <datog309@gmail.com>
2021-05-24 23:06:31 +0400

    first functional test done

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,12 +9,17 @@ let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
 
-    test("Translation with text and locale fields: POST request to /api/translate", () => {
-
+    test("Translation with text and locale fields: POST request to /api/translate", async () => {
+        const res = await chai.request(server)
+            .post("/api/translate")
+            .set("Content-Type", "application/json")
+            .send({ text: "Mangoes are my favorite fruit.", locale: "american-to-british" });
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, { text: "Mangoes are my favorite fruit.", tranlation: "Mangoes are my <span class=\"highlight\">favourite</span> fruit." });
     });
 
     test("Translation with text and invalid locale field: POST request to /api/translate", () => {
-
+        
     });
 
     test("Translation with missing text field: POST request to /api/translate", () => {
