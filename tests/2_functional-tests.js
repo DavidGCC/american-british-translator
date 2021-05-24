e467318eb31199db7f3d8bb3bc983cfa20788d9b
DavidG <datog309@gmail.com>
2021-05-24 23:15:45 +0400

    functional tests done

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -19,22 +19,47 @@ suite('Functional Tests', () => {
     });
 
     test("Translation with text and invalid locale field: POST request to /api/translate", () => {
-        
+        const res = await chai.request(server)
+            .post("/api/translate")
+            .set("Content-Type", "application/json")
+            .send({ text: "Mangoes are my favorite fruit.", locale: "something" });
+        assert.equal(res.status, 200);
+        assert.deepEqual(req.body, { error: "Invalid value for locale field" });
     });
 
     test("Translation with missing text field: POST request to /api/translate", () => {
-
+        const res = await chai.request(server)
+            .post("/api/translate")
+            .set("Content-Type", "application/json")
+            .send({ locale: "american-to-british" });
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, { error: "Required field(s) missing" });
     });
 
     test("Translation with missing locale field: POST request to /api/translate", () => {
-
+        const res = await chai.request(server)
+            .post("/api/translate")
+            .set("Content-Type", "application/json")
+            .send({ text: "Mangoes are my favorite fruit." });
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, { error: "Required field(s) missing" });
     });
 
     test("Translation with empty text: POST request to /api/translate", () => {
-
+        const res = await chai.request(server)
+            .post("/api/translate")
+            .set("Content-Type", "application/json")
+            .send({ text: "", locale: "american-to-british" });
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, { error: "No text to translate" });
     });
 
     test("Translation with text that needs no translation: POST request to /api/translate", () => {
-
+        const res = await chai.request(server)
+            .post("/api/translate")
+            .set("Content-Type", "application/json")
+            .send({ text: "Like a high tech Rube Goldberg machine.", locale: "american-to-british" });
+        assert.equal(res.status, 200);
+        assert.deepEqual({ text: "Like a high tech Rube Goldberg machine.", tranlation: "Everything looks good to me!" });
     });
 });
